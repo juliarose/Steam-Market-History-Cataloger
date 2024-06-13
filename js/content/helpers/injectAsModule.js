f865ee7924e5dfbc4dac04c5717434d0d0c07c27
juliarose <julialunarose@gmail.com>
2024-06-13 01:36:03 -0400

    Modified CSS so a dark theme would be easy to implement

--- js/content/helpers/injectAsModule.js
+++
@@ -1,5 +0,0 @@
-// this injects a script as a module
-// https://stackoverflow.com/questions/48104433/how-to-import-es6-modules-in-content-script-for-chrome-extension/53033388#53033388
-function injectAsModule(location) {
-    import(chrome.extension.getURL(location));
-}
